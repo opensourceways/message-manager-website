12a621cdec3ad669788993fdece18ad40031619e
Erotik <1723168479@qq.com>
2024-10-10 16:25:54 +0800

    fix: 删掉无用的interface

--- src/@types/type-messages.ts
+++ src/@types/type-messages.ts
@@ -24,37 +24,6 @@ export interface MessageT {
   is_read: boolean;
 }
 
-export interface CommonMsgQueryParamT {
-  /** 消息来源 */
-  source?: string;
-  /** 消息类型 */
-  event_type?: string;
-  /** 已读 */
-  is_read?: 1 | 0;
-  /** 页数 */
-  page?: number;
-  /** 页大小 */
-  count_per_page?: number;
-  /** 搜索gitee仓库/eur项目关键字 */
-  key_word?: string;
-  /** 是否特别关注消息 */
-  is_special?: string;
-}
-
-export interface EurMsgQueryParamT extends CommonMsgQueryParamT {
-  /** eur构建状态 */
-  build_status?: string;
-}
-
-export interface GiteeMsgQueryParamT extends CommonMsgQueryParamT {
-  /** gitee消息是否机器人 */
-  is_bot?: string;
-  sig?: string;
-  repos?: string;
-}
-
-export type MsgQueryParamT = EurMsgQueryParamT | GiteeMsgQueryParamT;
-
 export interface Sig {
   sig_name: string;
   repos: string[];
